7da475c49a653fb194f47fc91b5944a339e5d724
Danny Leshem <dleshem@gmail.com>
2016-03-31 10:20:50 +0300

    Added MercadoPago's "countryCode" field.

--- src/gateways/com.mercadopago.js
+++ src/gateways/com.mercadopago.js
@@ -6,6 +6,7 @@ module.exports = {
     form : [
         {type : "string", name : "clientId"},
         {type : "string", name : "clientSecret"},
+        {type : "country", name : "countryCode"}
     ],
 
 	// @see https://www.about-payments.com/knowledge-base/method/mercadopago#countries
