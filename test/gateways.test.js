04c08b3c328043ed84f4dc0c3cf282689c0f8f7b
Danny Leshem <dleshem@gmail.com>
2017-05-24 11:57:09 +0300

    More ES6.

--- test/gateways.test.js
+++ test/gateways.test.js
@@ -1,7 +1,6 @@
-var expect    = require('chai').expect;
-var _         = require("lodash");
-var gateways  = require("../src/gateways");
-var adapters  = require("../src/adapters");
+import {expect} from 'chai';
+import _ from 'lodash';
+import * as gateways from '../src/gateways';
 
 
 describe("gateways", () => {
@@ -9,14 +8,14 @@ describe("gateways", () => {
     
     describe("getGatewaysForCountry", () => {
         it ('returns the expected object when fetching Israel', () => {
-            var israeliGateways = gateways.getGatewaysForCountry("IL");
+            const israeliGateways = gateways.getGatewaysForCountry("IL");
             expect(israeliGateways).to.have.length.of(4);
         });
     });
 
     describe("getGatewayById", () => {
         it ('returns the expected object when fetching eway', () => {
-            var gateway = gateways.getGatewayById("com.ewaypayments");
+            const gateway = gateways.getGatewayById("com.ewaypayments");
             expect(gateway.id).to.equal("com.ewaypayments");
         });
     });
