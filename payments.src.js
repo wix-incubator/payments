4b33d68bc8af79b4b3f1f614e679d9c7602c91e2
builduser <builduser@wix.com>
2017-09-14 10:24:07 +0000

    bower version 2.0.455

--- payments.src.js
+++ payments.src.js
@@ -17386,7 +17386,7 @@ var PaymentsMethods =
 	    form: [{ type: 'string', name: 'login' }, { type: 'string', name: 'transactionKey' }],
 
 	    // @see http://www.authorize.net/international/
-	    countries: ['AD', 'AT', 'AU', 'BE', 'BG', 'CA', 'CH', 'CY', 'CZ', 'DE', 'DK', 'ES', 'FI', 'FR', 'GB', 'GI', 'GR', 'HU', 'IE', 'IT', 'LI', 'LU', 'MC', 'MT', 'NL', 'NO', 'NZ', 'PL', 'PT', 'RO', 'SE', 'SI', 'SK', 'SM', 'US', 'VA'],
+	    countries: ['AD', 'AT', 'AU', 'BE', 'BG', 'CA', 'CH', 'CY', 'CZ', 'DE', 'DK', 'ES', 'FI', 'FR', 'GB', 'GI', 'GR', 'GU', 'HU', 'IE', 'IT', 'LI', 'LU', 'MC', 'MT', 'NL', 'NO', 'NZ', 'PL', 'PT', 'RO', 'SE', 'SI', 'SK', 'SM', 'US', 'VA'],
 
 	    fields: [],
 
