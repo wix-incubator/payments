59c03c9bad85e85f3389ff48c52c2b9f8114c934
builduser <builduser@wix.com>
2018-03-28 02:22:14 +0000

    bower version 2.0.648

--- payments.src.js
+++ payments.src.js
@@ -17432,7 +17432,7 @@ var PaymentsMethods =
 /* 11 */
 /***/ (function(module, exports) {
 
-	var core = module.exports = { version: '2.5.3' };
+	var core = module.exports = { version: '2.5.4' };
 	if (typeof __e == 'number') __e = core; // eslint-disable-line no-undef
 
 
