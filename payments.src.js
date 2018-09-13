06264ebf70f6c9081f934a5ba566c3bef4533e88
builduser <builduser@wix.com>
2018-09-13 05:00:43 +0000

    bower version 2.0.825

--- payments.src.js
+++ payments.src.js
@@ -134,7 +134,7 @@ var PaymentsMethods =
 	  var undefined;
 
 	  /** Used as the semantic version number. */
-	  var VERSION = '4.17.10';
+	  var VERSION = '4.17.11';
 
 	  /** Used as the size to enable large array optimizations. */
 	  var LARGE_ARRAY_SIZE = 200;
@@ -398,7 +398,7 @@ var PaymentsMethods =
 	  var reHasUnicode = RegExp('[' + rsZWJ + rsAstralRange  + rsComboRange + rsVarRange + ']');
 
 	  /** Used to detect strings that need a more robust regexp to match words. */
-	  var reHasUnicodeWord = /[a-z][A-Z]|[A-Z]{2,}[a-z]|[0-9][a-zA-Z]|[a-zA-Z][0-9]|[^a-zA-Z0-9 ]/;
+	  var reHasUnicodeWord = /[a-z][A-Z]|[A-Z]{2}[a-z]|[0-9][a-zA-Z]|[a-zA-Z][0-9]|[^a-zA-Z0-9 ]/;
 
 	  /** Used to assign default `context` object properties. */
 	  var contextProps = [
@@ -1344,20 +1344,6 @@ var PaymentsMethods =
 	      }
 	    }
 	    return result;
-	  }
-
-	  /**
-	   * Gets the value at `key`, unless `key` is "__proto__".
-	   *
-	   * @private
-	   * @param {Object} object The object to query.
-	   * @param {string} key The key of the property to get.
-	   * @returns {*} Returns the property value.
-	   */
-	  function safeGet(object, key) {
-	    return key == '__proto__'
-	      ? undefined
-	      : object[key];
 	  }
 
 	  /**
@@ -3817,7 +3803,7 @@ var PaymentsMethods =
 	          if (isArguments(objValue)) {
 	            newValue = toPlainObject(objValue);
 	          }
-	          else if (!isObject(objValue) || (srcIndex && isFunction(objValue))) {
+	          else if (!isObject(objValue) || isFunction(objValue)) {
 	            newValue = initCloneObject(srcValue);
 	          }
 	        }
@@ -6738,6 +6724,22 @@ var PaymentsMethods =
 	        array[length] = isIndex(index, arrLength) ? oldArray[index] : undefined;
 	      }
 	      return array;
+	    }
+
+	    /**
+	     * Gets the value at `key`, unless `key` is "__proto__".
+	     *
+	     * @private
+	     * @param {Object} object The object to query.
+	     * @param {string} key The key of the property to get.
+	     * @returns {*} Returns the property value.
+	     */
+	    function safeGet(object, key) {
+	      if (key == '__proto__') {
+	        return;
+	      }
+
+	      return object[key];
 	    }
 
 	    /**
