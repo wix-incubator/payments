07adcfcc3ef7bf63779be032ed2bb88e441a71bf
builduser <buildserver@wix.com>
2020-06-08 10:16:17 +0000

    bower version 2.0.1330

--- payments.src.js
+++ payments.src.js
@@ -17862,7 +17862,7 @@ var PaymentsMethods =
 	    form: [{ type: 'string', name: 'apiKey' }],
 
 	    // @see https://stripe.com/global
-	    countries: ['AT', 'AU', 'BE', 'BR', 'CA', 'CH', 'DE', 'DK', 'ES', 'FI', 'FR', 'GB', 'HK', 'IE', 'IT', 'JP', 'LU', 'MX', 'NL', 'NO', 'NZ', 'PT', 'SE', 'SG', 'US'],
+	    countries: ['AT', 'AU', 'BE', 'BR', 'CA', 'CH', 'DE', 'DK', 'ES', 'FI', 'FR', 'GB', 'HK', 'IE', 'IT', 'JP', 'LU', 'MX', 'NL', 'NO', 'NZ', 'PT', 'SE', 'SG', 'US', 'PR'],
 
 	    fields: [],
 
